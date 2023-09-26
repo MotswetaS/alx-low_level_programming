3e895cce64c0dc66f52833945968cde512548ddb
Rebonye <motsweat04@gmail.com>
2023-09-26 14:09:24 -0700

    removed trailing space

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -4,7 +4,7 @@
  * reverse_listint - we are reversing a linked list
  * @head: pointer to the first node in the list
  *
- * Return: returns an addressmof the recersed list the new 
+ * Return: returns an addressmof the recersed list the new
  */
 listint_t *reverse_listint(listint_t **head)
 {
