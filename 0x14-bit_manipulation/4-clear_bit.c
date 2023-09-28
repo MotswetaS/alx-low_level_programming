38860208d0788ffcb37bda10f4acacf476822a01
Rebonye <motsweat04@gmail.com>
2023-09-28 12:30:53 -0700

    removed trailing white space

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,7 +2,7 @@
 
 /**
  * clear_bit - we are setting the value of a bit to 0 qt any given index
- * @n: pointer to the number 
+ * @n: pointer to the number
  * @index: start index from zero
  *
  * Return: 1 - success , -1 ifailure
