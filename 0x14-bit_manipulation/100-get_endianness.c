1823e263863bc27892bd02093e5cccb67899ccc7
Rebonye <motsweat04@gmail.com>
2023-09-28 12:43:32 -0700

    fixing character number

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * get_endianness - wit thi sfunction we are checking the endianess of a function
+ * get_endianness - with this function we are 
+ * checking the endianess of a function
  * Return: 0 is  big endian, 1 is little endian
  */
 int get_endianness(void)
