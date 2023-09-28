4292253c7279f4ed2b5b5d3949fc1faa1846be08
Rebonye <motsweat04@gmail.com>
2023-09-28 12:45:36 -0700

    removed trailing white space

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_endianness - with this function we are 
+ * get_endianness - with this function we are
  * checking the endianess of a function
  * Return: 0 is  big endian, 1 is little endian
  */
