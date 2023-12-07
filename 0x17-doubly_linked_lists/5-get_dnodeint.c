439334c44ccf43a26d16796ebcc9518161a91187
Rebonye <motsweta04@gmail.com>
2023-12-07 05:41:00 -0800

    fix

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -3,7 +3,7 @@
 /**
  * get_dnodeint_at_index - function will return nth node
  * of a dlistint_t linked list.
- * @head: pointer to head 
+ * @head: pointer to head
  * @index: is the node to search for, starting from 0
  * Return: nth node or null
  **/
